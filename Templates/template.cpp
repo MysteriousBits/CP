d992aa93b31fbba8db628a401a6044daf5340b7d
ishraq islam <21dcba12@gmail.com>
2022-05-12 23:36:17 +0600

    template initial

---
+++ Templates/template.cpp
@@ -0,0 +1,45 @@
+/***** Template headers + macros *****/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+using ordered_set =  tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> VI;
+typedef pair<int, int> PII;
+
+#define endl "\n"
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+#define MAX 200002
+
+/**********************************/
+
+void solve()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        // Starts here...
+        cout << "hlo!";
+        return;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
