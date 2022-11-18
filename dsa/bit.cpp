45c0533d04065a15650f3eebb57b2b1637a56cc0
MysteriousBits <21dcba12@gmail.com>
2022-11-18 15:48:46 +0600

    dsu+bit

---
+++ dsa/bit.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+// struct imple
+struct bit
+{
+    int n;
+    vector<ll> tree;
+
+    bit(int s)
+    {
+        n = s;
+        tree.assign(n, 0);
+    }
+
+    ll get(int i)
+    {
+        ll res = 0;
+        for(; i >= 0; i -= i & -i) res += tree[i];
+        return res;
+    }
+
+    void add(int i, int x)
+    {
+        for (; i < n; i += i & -i) tree[i] += x;
+    }
+    
+    ll rsq(int l, int r)
+    {
+        return get(r) - get(l - 1);
+    }
+};
+
+
+// non-struct imple
+inline ll get(int i, vecotr<ll>& tree)
+{
+    ll res = 0;
+    for(; i >= 0; i -= i & -i) res += tree[i];
+    return res;
+}
+
+inline void add(int i, int x, vector<ll>& tree)
+{
+    for (; i < n; i += i & -i) tree[i] += x;
+}
---
+++ dsa/dsu.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// struct imple
+struct dsu
+{
+    int *parent, *size;
+ 
+    dsu(int n)
+    {
+        parent = new int[n + 1];
+        size = new int[n + 1];
+ 
+        for (int i = 1; i <= n; ++i)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    ~dsu()
+    {
+        delete[] parent;
+        delete[] size;
+    }
+
+    int find(int u)
+    {
+        if (parent[u] == u) return u;
+        return parent[u] = find(parent[u]);
+    }
+ 
+    void merge(int u, int v)
+    {
+        u = find(u);
+        v = find(v);
+ 
+        if (u != v)
+        {
+            if (size[u] < size[v]) swap(u, v);
+            parent[v] = u;
+            size[u] += size[v];
+        }
+    }
+};
+
+
+// Nonstruct imple
+int find(int n, int parent[])
+{
+    if (parent[u] == u) return u;
+    return parent[u] = find(parent[u]);
+}
+
+void merge(int u, int v, int parent[], int size[])
+{
+    u = find(u, parent);
+    v = find(v, parent);
+
+    if (u != v)
+    {
+        if (size[u] < size[v]) swap(u, v);
+        parent[v] = u;
+        size[u] += size[v];
+    }
+}
+
+
+// vector imple
+// parent = 0 for roots
+int find(int n, vecotr<int>& a)
+{
+    if (!a[u]) return u;
+    return a[u] = find(a[u]);
+}
+
+void merge(int u, int v, vector<int>& a, vector<int>& size)
+{
+    u = find(u, a);
+    v = find(v, a);
+
+    if (u != v)
+    {
+        if (size[u] < size[v]) swap(u, v);
+        a[v] = u;
+        size[u] += size[v];
+    }
+}
