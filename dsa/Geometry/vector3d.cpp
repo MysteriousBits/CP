54227c3c37eed52a8fcbadc948a83e5799afbb4d
MysteriousBits <si.islam214@gmail.com>
2026-02-23 01:53:10 +0600

    Fixed feq

--- dsa/Geometry/vector3d.cpp
+++ dsa/Geometry/vector3d.cpp
@@ -4,12 +4,12 @@ using namespace std;
 #define EPS 1e-12
 #define pi 3.141592653589793115997963468544185161590576171875L
 
-bool feq(long double a, long double b)
+bool feq(const long double& a, const long double& b)
 {
     long double del = fabsl(a - b);
     if (del <= EPS) return true;
 
-    return del <= EPS * max({1.0L, fabsl(a), fabsl(b)});
+    return del <= EPS * max(fabsl(a), fabsl(b));
 }
 
 struct vector3d
