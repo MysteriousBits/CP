ddc56556d07143fbe4203bb03be9c8ea17ef59e7
MysteriousBits <si.islam214@gmail.com>
2026-04-22 23:04:35 +0600

    added random generator

---
+++ dsa/Misc/rand.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int randint(int l, int r) {
+    uniform_int_distribution<int> dist(l, r);
+    return dist(rng);
+}
