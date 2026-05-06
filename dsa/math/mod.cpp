3d70c45e35d2de680574b43e2dda8695ee65a8b8
MysteriousBits <si.islam214@gmail.com>
2026-05-06 20:57:00 +0600

    Updated mod template

--- dsa/math/mod.cpp
+++ dsa/math/mod.cpp
@@ -5,56 +5,54 @@ typedef long long ll;
 #define MOD 998244353
 #define MAXN 100001
 
-ll fact[MAXN];
-
-void calc_fact(ll mod = MOD)
+inline ll mod(ll n, ll m = MOD)
 {
-    fact[0] = 1;
-    for (int i = 1; i < MAXN; ++i)
-    {
-        fact[i] = (fact[i - 1] * i) % mod;
-    }
+    return (m + n % m) % m;
 }
 
-ll mod(ll n, ll mod = MOD)
-{
-    n %= mod;
-    return n < 0 ? mod + n : n;
-}
-
-ll modpow(ll n, ll k, ll mod = MOD)
+ll modpow(ll n, ll k, ll m = MOD)
 {
     ll ret = 1;
-
+    n = mod(n, m);
     while (k)
     {
-        if (k & 1) ret = (ret * n) % mod;
-        n = (n * n) % mod;
+        if (k & 1) ret = (ret * n) % m;
+        n = (n * n) % m;
         k >>= 1;
     }
 
     return ret;
 }
 
-inline ll inv(ll n, ll mod = MOD)
+inline ll inv(ll n, ll m = MOD)
 {
-    return modpow(n, mod - 2, mod);
+    return modpow(n, m - 2, m);
 }
 
-inline ll mul(ll x, ll y, ll mod = MOD)
+inline ll mul(ll x, ll y, ll m = MOD)
 {
-    return ((x % mod) * (y % mod)) % mod;
+    return (mod(x, m) * mod(y, m)) % m;
 }
 
-inline ll dvd(ll x, ll y, ll mod = MOD)
+inline ll dvd(ll x, ll y, ll m = MOD)
 {
-    return mul(x, inv(y, mod), mod);
+    return mul(x, inv(y, m), m);
 }
 
-ll C(int n, int k, ll mod = MOD)
+// Binomial coeffecient
+ll fact[MAXN];
+void calc_fact(ll m = MOD)
+{
+    fact[0] = 1;
+    for (int i = 1; i < MAXN; ++i)
+    {
+        fact[i] = (fact[i - 1] * i) % m;
+    }
+}
+
+ll C(int n, int k, ll m = MOD)
 {
     if (k > n) return 0;
     
-    return (1LL * fact[n] *
-        inv((1LL * fact[k] * fact[n - k]) % mod)) % mod;
+    return dvd(fact[n], fact[k] * fact[n - k] % m, m);
 }
