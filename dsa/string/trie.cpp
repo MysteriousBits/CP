66a0a840238123279b8a9a9ef5ccd4669b061524
MysteriousBits <si.islam214@gmail.com>
2026-07-07 01:13:40 +0600

    Added size to string trie

--- dsa/string/trie.cpp
+++ dsa/string/trie.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 struct trienode
 {
+    int size;
     bool endmark;
     vector<int> child;
 
     trienode(int sz)
     {
+        size = 0;
         endmark = false;
         child.resize(sz, 0);
     }
@@ -21,6 +23,7 @@ struct trie
     char fst;
     vector<trienode> nodes;
 
+    // 26, 'a' for lowercase latin letters
     trie(int alpha_sz, char alpha_start)
     {
         sz = alpha_sz;
@@ -41,10 +44,12 @@ struct trie
                 nodes.pb(trienode(sz));
             }
 
+            nodes[cur].size++;
             cur = nodes[cur].child[c - fst];
         }
 
         nodes[cur].endmark = true;
+        nodes[cur].size++;
     }
 
     bool search(string& s)
@@ -52,29 +57,27 @@ struct trie
         int cur = 0;
         for (char c : s)
         {
-            if (!nodes[cur].child[c - fst])
+            if (!nodes[cur].child[c - fst] || !nodes[cur].size)
                 return false;
 
             cur = nodes[cur].child[c - fst];
         }
 
-        return nodes[cur].endmark; 
+        return nodes[cur].endmark;
     }
 
     bool erase(string& s)
     {
+        if (!search(s)) return false;
+
         int cur = 0;
         for (char c : s)
         {
-            if (!nodes[cur].child[c - fst])
-                return false;
-
+            nodes[cur].size--;
             cur = nodes[cur].child[c - fst];
         }
 
-        if (!nodes[cur].endmark) return false;
-
-        nodes[cur].endmark = false;
+        if (!--nodes[cur].size) nodes[cur].endmark = false;
         return true;
     }
 };
